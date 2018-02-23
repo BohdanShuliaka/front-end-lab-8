cf13a0a91acb2e1bed97622fd77b39cf3979db67
BohdanShuliaka <bohdan.shuliaka@gmail.com>
2018-02-23 18:46:16 +0200

    not the last

--- homework_10/task2.js
+++ homework_10/task2.js
@@ -30,39 +30,66 @@ function showResult(fighter) {
   console.log('- Combat stats:', fighter.getCombatHistory());
   console.log('- Properties:', fighter.getStats());
 }
-
-/**
- * Your code goes here
- * function foo() { ... }
- */
+function fighter(commonFighter) {
+  let combatHistory = {
+      wins: 0,
+      loses: 0
+  };
+  return {
+      getName: function() {
+        return commonFighter.name;
+      },
+      block: function() {
+        return (Math.random() > 0.5);
+      },
+      getStats: function() {
+        return commonFighter;
+      },
+      getCombatHistory: function() {
+        return combatHistory;
+      },
+      fight: function(defender) {
+        if (commonFighter.block) {
+          return false;
+        } else {
+          defender.getStats().hp -= commonFighter.attack;
+            if (commonFighter.attack >= defender.getStats().hp) {
+            combatHistory.wins++;
+            defender.getCombatHistory().loses++;
+            }
+          return true;
+        }
+      }
+    }
+}
 
 /**
  * The following code must be valid after implementation!
  */ 
 
-// var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
-// var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
-// var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
+var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
+var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
+var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
 
-// fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
-// fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
+fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
+fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
 
 // /**
 //  * Fighter John
 //  * - Combat stats: { wins: 1, loses: 0 }
 //  * - Properties: { name: 'John', attack: 100, hp: 100 }
 //  */
-// showResult(fighter1);
+showResult(fighter1);
 
 // /** Fighter Kayn
 //  * - Combat stats: { wins: 0, loses: 0 }
 //  * - Properties: { name: 'Kayn', attack: 50, hp: 200 }
 //  */
-// showResult(fighter2); 
+showResult(fighter2); 
 
 // /**
 //  * Fighter Bill
 //  * - Combat stats: { wins: 0, loses: 1 }
 //  * - Properties: { name: 'Bill', attack: 40, hp: 0 }
 //  */
-// showResult(fighter3);
+showResult(fighter3);
